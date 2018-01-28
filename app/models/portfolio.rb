0b422029d4dfe34ad52fd9edbddae20b5cff8bbc
clayterry <lclaytont@gmail.com>
2018-01-27 20:38:02 -0500

    Implements image defaults for portfolios

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,6 +1,7 @@
 class Portfolio < ApplicationRecord
   validates_presence_of :title, :body, :main_image, :thumb_image
 
+  # Examples of custom scope i.e. db relationships
   # Looks like ruby
   def self.angular
     where(subtitle: "Angular")
@@ -10,4 +11,10 @@ class Portfolio < ApplicationRecord
   scope :ruby_on_rails, -> {
     where(subtitle: "Ruby on Rails")
   }
+
+  after_initialize :set_defaults
+  def set_defaults
+    self.main_image ||= "http://via.placeholder.com/600x400"
+    self.thumb_image ||= "http://via.placeholder.com/350x150"
+  end
 end
