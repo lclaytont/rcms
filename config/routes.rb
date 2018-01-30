f55a5c3295389a97c2d9f7a7f00d15b6c2abec46
clayterry <lclaytont@gmail.com>
2018-01-29 22:26:29 -0500

    Implements Basic Auth

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :portfolios, except: [:show]
   get 'portfolio/:id', to: 'portfolios#show', as: "portfolio_show"
   get 'portfolios/angular', to: 'portfolios#angular', as: "portfolio_angular"
